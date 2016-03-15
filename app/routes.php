346fab9d3450a13aa915fca73dad4465885582e9
jakkarinv1 <jakkarinwebmaster@gmail.com>
2016-03-15 10:02:39 +0700

    Update code.

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ Route::pattern('string', '[A-z0-9]+');
 Route::controller('controller', 'mainController');
 
 Route::get('', 'mainController@index');
+Route::post('', 'mainController@index');
 Route::get('adminmsd/{numberic}', 'mainController@numberic');
 Route::get('adminmsd/{letter}', 'mainController@letter');
 
